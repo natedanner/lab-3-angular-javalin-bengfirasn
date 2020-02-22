5c7a7e8b14e99c369095eddce3b893bdb03d4fd0
Firas Naber <naber024@morris.umn.edu>
2020-02-22 16:27:49 -0600

    Finish tests
    Complete generating 100% test coverage for ts.service, for issue #10.

--- client/src/app/todos/todo.service.spec.ts
+++ client/src/app/todos/todo.service.spec.ts
@@ -3,6 +3,8 @@ import { HttpClientTestingModule, HttpTestingController } from '@angular/common/
 import { TestBed } from '@angular/core/testing';
 import { Todo } from './todo';
 import { TodoService } from './todo.service';
+import { stat } from 'fs';
+import { UserService } from '../users/user.service';
 
 describe('Todo service', () => {
     const testTodos: Todo[] = [
@@ -68,7 +70,63 @@ describe('Todo service', () => {
         // triggers the subscribe above, which leads to that check
         // actually being performed.
         req.flush(testTodos);
-
     });
 
-});
+    it('getTodos calls api/todos with filter parameter \'status\'', () => {
+      todoService.getTodos({status: false}).subscribe(
+        todos => expect(todos).toBe(testTodos)
+      );
+      const req = httpTestingController.expectOne(
+        (request) => request.url.startsWith(todoService.todoUrl) && request.params.has('status')
+      );
+
+      expect(req.request.method).toEqual('GET');
+      expect(req.request.params.get('status').toLowerCase).toEqual('false');
+      req.flush(testTodos);
+    });
+
+    it('getTodoByID() calls api/todos/id', () => {
+      const targetTodo: Todo = testTodos[1];
+      const targetId: string = targetTodo._id;
+      todoService.getTodoById(targetId).subscribe(
+        todo => expect(todo).toBe(targetTodo)
+      );
+
+      const expectedUrl: string = todoService.todoUrl + '/' + targetId;
+      const req = httpTestingController.expectOne(expectedUrl);
+      expect(req.request.method).toEqual('GET');
+      req.flush(targetTodo);
+    });
+
+    it('filterTodos() filters by owner', () => {
+      expect(testTodos.length).toBe(4);
+      const todoOwner = 'Joe';
+      expect(todoService.filterTodos(testTodos, { owner: todoOwner }).length).toBe(2);
+    });
+
+    it('filterTodos() filters by body', () => {
+      expect(testTodos.length).toBe(4);
+      const todoBody = 'the';
+      expect(todoService.filterTodos(testTodos, { body: todoBody }).length).toBe(2);
+    });
+
+    it('filterTodos() filters by category', () => {
+      expect(testTodos.length).toBe(4);
+      const todoCategory = 'Home';
+      expect(todoService.filterTodos(testTodos, { category: todoCategory }).length).toBe(2);
+    });
+
+    it('filterTodos() filters by multiple parameters', () => {
+      expect(testTodos.length).toBe(4);
+      const todoOwner = 'Joe';
+      const todoCategory = 'Home';
+      expect(todoService.filterTodos(testTodos, { owner: todoOwner, category: todoCategory }).length).toBe(1);
+    });
+
+    it('filterTodos() filters by multiple exclusive parameters', () => {
+      expect(testTodos.length).toBe(4);
+      const todoOwner = 'Jane';
+      const todoCategory = 'Groceries';
+      expect(todoService.filterTodos(testTodos, { owner: todoOwner, category: todoCategory }).length).toBe(0);
+    });
+  });
